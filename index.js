abaa36823cb8626f6f4d92e0e2dac00536ee0872
Team-Nope <65626203+Team-Nope@users.noreply.github.com>
2020-07-18 21:06:50 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+client.login("null")
+client.on('ready', () => {
+  if (0 == 1 + 1) {
+    for (var count = 0; count < 10; count++) {
+      //Make sure all loops have a break function. If you're loop can't break, it risks being a infinite loop, which can crash your bot.
+    }
+  }
+  if (0 == 1 + 1) {
+    for (var count2 = 0; count2 < 10; count2++) {
+      //Make sure all loops have a break function. If you're loop can't break, it risks being a infinite loop, which can crash your bot.
+    }
+  }
+})
